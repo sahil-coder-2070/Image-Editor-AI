0bb973f18998bbc40777751a9e0c2b9e72e6460e
Sahil Singh <sahil171759@gmail.com>
2026-01-26 02:39:27 +0530

    prettirerc added for class shorting

--- components/layouts/hero.tsx
+++ components/layouts/hero.tsx
@@ -7,40 +7,40 @@ import { ArrowRight } from "lucide-react";
 
 const HeroSection = () => {
   return (
-    <Container className=" pt-15 pb-20 text-center min-h-screen flex flex-col items-center justify-center relative  overflow-hidden">
-      <div className="w-full flex items-center justify-center">
-        <div className=" border-2 shadow-red-400/40 px-4 py-1 gap-2 rounded-2xl shadow-sm flex items-center">
+    <Container className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden pt-15 pb-20 text-center">
+      <div className="flex w-full items-center justify-center">
+        <div className="flex items-center gap-2 rounded-2xl border-2 px-4 py-1 shadow-sm shadow-red-400/40">
           <AnimatedShinyText>
             For fast moving engineering teams
           </AnimatedShinyText>
           <ArrowRight
             size={18}
-            className=" text-red-600/70 dark:text-orange-400/70"
+            className="text-red-600/70 dark:text-orange-400/70"
           />
         </div>
       </div>
 
-      <div className="max-w-4xl mx-auto px-6 mt-6">
-        <h1 className="text-center text-3xl font-medium tracking-tight text-black md:text-4xl lg:text-6xl dark:text-white mt-4">
+      <div className="mx-auto mt-6 max-w-4xl px-6">
+        <h1 className="mt-4 text-center text-3xl font-medium tracking-tight text-black md:text-4xl lg:text-6xl dark:text-white">
           Simple Tools for <br />
           Better
-          <span className="text-red-600/80 font-semibold dark:text-orange-600">
+          <span className="font-semibold text-red-600/80 dark:text-orange-600">
             {" "}
             Images
           </span>
         </h1>
-        <p className="text-xs md:text-base mx-auto max-w-lg text-neutral-500 dark:text-neutral-400 mb-8 leading-tight mt-6">
+        <p className="mx-auto mt-6 mb-8 max-w-lg text-xs leading-tight text-neutral-500 md:text-base dark:text-neutral-400">
           Edit images easily using AI and intuitive tools built for{" "}
-          <span className="text-black dark:text-white font-medium">speed</span>,{" "}
-          <span className="text-black dark:text-white font-medium">
+          <span className="font-medium text-black dark:text-white">speed</span>,{" "}
+          <span className="font-medium text-black dark:text-white">
             quality
           </span>
           , and a{" "}
-          <span className="text-black dark:text-white font-medium">smooth</span>{" "}
+          <span className="font-medium text-black dark:text-white">smooth</span>{" "}
           editing experience.
         </p>
 
-        <div className="flex  sm:flex-row gap-6 justify-center items-center mb-12">
+        <div className="mb-12 flex items-center justify-center gap-6 sm:flex-row">
           <Link href={"/dashboard"}>
             <Button variant="custom">Start Creating</Button>
           </Link>
